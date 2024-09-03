544b6689c1722c201c36237cd147d26b4ba784ba
hemadharshini2003 <hemadharshini5678@gmail.com>
2024-09-03 12:42:37 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,15 @@
+package hemadharshini;
+
+public class variable {
+	 public static void main(String[] args) {  
+	        int a=21,salary=50000;
+	        String name="PREM",dept="DATA ANALYST";
+	        System.out.println("The employee name is:"+name);
+	        System.out.println("The employee age is:"+a);
+	        System.out.println("The employee dept is:"+dept);
+	        System.out.println("The employee salary is:"+salary);
+	        
+	        
+	        
+}
+}
